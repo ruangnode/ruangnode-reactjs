99bdd54823956e0dc55e6059a81d69e6d13d488b
bakkeieth <lukmanhabibie009@gmail.com>
2023-07-04 13:55:19 +0700

    fixing footer

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -7,7 +7,7 @@ export const links = [
   { id: 1, link: '#', title: 'Home' },
   { id: 2, link: '#about', title: 'About' },
   { id: 3, link: '#services', title: 'Services' },
-  { id: 4, link: '#portfolio', title: 'Portfolio' },
+  { id: 4, link: '#portfolio', title: 'Networks' },
   { id: 5, link: '#contact', title: 'Contact' },
 ];
 
