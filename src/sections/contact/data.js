71d03c74cc5811d701ed73dacbf7bf18c716fe89
bakkeieth <lukmanhabibie009@gmail.com>
2023-07-09 23:53:17 +0700

    fix telegram account

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -4,7 +4,7 @@ import { FaTelegram } from 'react-icons/fa';
 
 const data = [
   { id: 1, icon: <HiOutlineMail />, link: 'mailto:lukmanhabibie009@gmail.com' },
-  { id: 2, icon: <FaTelegram />, link: 'https://t.me/bakkeieth' },
+  { id: 2, icon: <FaTelegram />, link: 'https://t.me/ruangnode' },
   // { id: 3, icon: <FaDiscord />, link: 'https://discord.gg/tTjDAfkCNn' },
 ];
 
