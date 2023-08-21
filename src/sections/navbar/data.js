6b3c026fdda85fe1dfaae6a2e4efd60ee55a9ae2
bakkeieth <lukmanhabibie009@gmail.com>
2023-08-21 16:49:08 +0700

    docs services

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -1,9 +1,9 @@
 const data = [
   { id: 1, link: '#', title: 'Home' },
-  // { id: 2, link: '#about', title: 'Why Ruangnode ?' },
   { id: 3, link: '#services', title: 'Services' },
   { id: 4, link: '#portfolio', title: 'Networks' },
   { id: 5, link: '#contact', title: 'Contact' },
+  { id: 2, link: 'https://docs.ruangnode.com/', title: 'Docs Services' },
 ];
 
 export default data;
