d9991831d2078b2dee32e9ad9eb2a32a854b3252
bakkeieth <lukmanhabibie009@gmail.com>
2023-08-24 08:56:38 +0700

    update portfolio

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -7,7 +7,7 @@ const Project = ({ project }) => {
         <img src={project.image} alt="Portfolio Project Image" />
       </div>
       <h4>{project.title}</h4>
-      <p>{project.desc}</p>
+      <p>Staked Token : {project.total}</p>
       <div className="portfolio__project-cta">
         <a href={project.health} className="btn sm" target="_blank" rel="noopner noreferrer">
           Health Check
